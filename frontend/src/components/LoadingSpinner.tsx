edf0ee72d900f58c2bf0c554de0ce27ceff66d02
Phillip Marashian <phillip.marashian@gmail.com>
2026-02-15 21:51:27 -0500

    make it centered

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { IonSpinner, IonContent } from '@ionic/react';
+import { IonSpinner } from '@ionic/react';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <IonContent
+    <div
       style={{
+        position: 'fixed',
+        inset: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
-        margin: 0,
-        padding: 0,
+        backgroundColor: 'var(--ion-background-color)',
+        zIndex: 9999,
       }}
     >
       <IonSpinner name="circular" style={{ transform: 'scale(1.5)' }} />
-    </IonContent>
+    </div>
   );
 };
 
